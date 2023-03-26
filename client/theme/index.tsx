1ca2b5afcf180bac2424f0a483673f098f9fbc7f
Jaehyeon Park <sawogus29@gmail.com>
2023-03-26 23:26:47 +0900

    feat(client/theme): Update color palette

--- client/theme/index.tsx
+++ client/theme/index.tsx
@@ -1,4 +1,6 @@
+import { color } from '@rneui/base';
 import { createTheme } from '@rneui/themed';
+import colors from './colors';
 
 const defaultFontFamily = 'pretendard'; // should be pretendard
 
@@ -19,23 +21,24 @@ const theme = createTheme({
     },
   },
   lightColors: {
-    primary: '#245DE5',
-    // secondary;
-    // background;
-    // white;
-    // black;
-    // grey0;
-    // grey1;
-    // grey2;
-    // grey3;
-    // grey4;
-    // grey5;
+    primary: colors.AI_BLUE,
+    secondary: colors.TECH_AQUA,
+    background: colors.GREY900,
+    white: colors.GREY900,
+    black: colors.GREY000,
+    grey0: colors.GREY800,
+    grey1: colors.GREY700,
+    grey2: colors.GREY500,
+    grey3: colors.GREY450,
+    grey4: colors.GREY400,
+    grey5: colors.GREY300,
     // greyOutline;
-    // searchBg;
-    // success;
-    // error;
-    // warning;
-    // divider;
+    searchBg: colors.GREY900,
+    success: colors.SUCCESS,
+    error: colors.DANGEROUS,
+    warning: colors.WARNING,
+    divider: colors.GREY700,
+    disabled: colors.GREY600,
   },
   mode: 'light',
 });
