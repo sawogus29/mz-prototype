b534947b9f39c4240c521b754a2806c23ed3b57c
Jaehyeon Park <sawogus29@gmail.com>
2023-03-30 17:17:52 +0900

    chore(client/welcome): Use default Appbar

--- client/app/(main)/_layout.tsx
+++ client/app/(main)/_layout.tsx
@@ -1,18 +1,12 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
 import MeerzetLogo from '../../assets/images/logo_medium.svg';
-import colors from '../../theme/colors';
 
 export default function MainLayout() {
   return (
     <Stack
       screenOptions={{
-        // headerTitle: () => <MeerzetLogo width={120} height={20} />,
-        header: () => (
-          <View style={{ padding: 16, backgroundColor: colors.GREY900 }}>
-            <MeerzetLogo width={120} height={20} style={{}} />
-          </View>
-        ),
+        headerTitle: () => <MeerzetLogo width={120} height={20} />,
+        headerShadowVisible: false,
       }}
     />
   );
