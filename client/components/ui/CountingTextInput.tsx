b7672535520859a472eab869c4d39de756ba7d15
Jaehyeon Park <sawogus29@gmail.com>
2023-04-01 16:48:38 +0900

    refactor(client/CountingTextInput): seperate styling

--- client/components/ui/CountingTextInput.tsx
+++ client/components/ui/CountingTextInput.tsx
@@ -1,15 +1,8 @@
-import {
-  Text,
-  StyleSheet,
-  TextInput,
-  Platform,
-  View,
-  TextInputProps,
-} from 'react-native';
-import { Input } from '@rneui/themed';
-import { useRef, useState } from 'react';
+import { Text, StyleSheet, View, TextInputProps } from 'react-native';
+import { useState } from 'react';
 import colors from '../../theme/colors';
 import { defaultFontFamily } from '../../theme';
+import TextInput from './TextInput';
 
 export interface CountingTextInputProps extends TextInputProps {
   maxLength: number;
@@ -17,34 +10,27 @@ export interface CountingTextInputProps extends TextInputProps {
 
 export default function CountingTextInput(props: CountingTextInputProps) {
   const [enteredText, setEnteredText] = useState('');
-  const [focoused, setFocused] = useState(false);
   const { maxLength } = props;
 
   return (
-    <>
-      <View style={styles.containerStyle}>
-        <TextInput
-          {...props}
-          style={[
-            styles.input,
-            //@ts-ignore
-            { outline: 'transparent' }, // for web
-            focoused && styles.focusedInput,
-            props.style,
-          ]}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          onChangeText={(text) => {
-            props.onChangeText && props.onChangeText(text);
-            setEnteredText(text);
-          }}
-          placeholderTextColor={colors.GREY500}
-        />
-        <Text style={styles.counterText}>
-          {enteredText.length}/{maxLength}
-        </Text>
-      </View>
-    </>
+    <View style={styles.containerStyle}>
+      <TextInput
+        {...props}
+        style={[
+          styles.input,
+          //@ts-ignore
+          props.style,
+        ]}
+        onChangeText={(text) => {
+          props.onChangeText && props.onChangeText(text);
+          setEnteredText(text);
+        }}
+        placeholderTextColor={colors.GREY500}
+      />
+      <Text style={styles.counterText}>
+        {enteredText.length}/{maxLength}
+      </Text>
+    </View>
   );
 }
 
@@ -53,20 +39,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  focusedInput: {
-    borderColor: colors.AI_BLUE,
-  },
   input: {
     // backgroundColor: 'red',
-    paddingVertical: 10,
-    paddingLeft: 12,
-    paddingRight: 50,
     width: '100%',
-    fontSize: 18,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: colors.GREY600,
-    fontFamily: defaultFontFamily,
+    paddingRight: 50,
   },
   counterText: {
     // backgroundColor: 'blue',
