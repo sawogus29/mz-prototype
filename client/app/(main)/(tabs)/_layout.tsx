75cbdda7dc99aa599d48396ed0baa32ec9fa85f9
Jaehyeon Park <sawogus29@gmail.com>
2023-04-15 15:19:15 +0900

    fix(client/tabs): remove scrollbar of tabs navigator

--- client/app/(main)/(tabs)/_layout.tsx
+++ client/app/(main)/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, Tabs, useFocusEffect } from 'expo-router';
 import { useEffect } from 'react';
 import { useAuth } from '../../../store/auth/auth-context';
+import { View } from 'react-native';
 
 export const unstable_settings = {
   initialRouteName: 'home',
@@ -11,5 +12,10 @@ export default function TabsLayout() {
   // useEffect(() => {
   //   if (user?.firstTime) unsetFirstTime();
   // }, [user?.firstTime]);
-  return <Tabs />;
+  return (
+    // added <View> to fix 'SCROLLBAR APPEARING BUG' in web browser
+    <View style={{ flex: 1, overflow: 'hidden' }}>
+      <Tabs />
+    </View>
+  );
 }
